71a65b249e544a56307b3b7d3855480f9d71ae8d
omnia <omnia.z.talaab@gmail.com>
2022-09-25 19:22:40 +0200

    upDate

--- lib/feature/home/presentation/widgets/home_widget.dart
+++ lib/feature/home/presentation/widgets/home_widget.dart
@@ -1,24 +1,28 @@
 import 'package:booking/core/util/widget_functions.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../core/util/blocs/app/cubit.dart';
 import '../../../../core/util/blocs/app/states.dart';
+import '../../../../core/util/widgets/formField/Form.dart';
 
 class HomeWidget extends StatefulWidget {
+
   const HomeWidget({Key? key}) : super(key: key);
 
   @override
   State<HomeWidget> createState() => _HomeWidgetState();
 }
 
+
 class _HomeWidgetState extends State<HomeWidget> {
+
   @override
   void initState() {
     super.initState();
   }
-
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<AppBloc, AppStates>(
@@ -30,25 +34,34 @@ class _HomeWidgetState extends State<HomeWidget> {
         }
       },
       builder: (context, state) {
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            MaterialButton(
-              onPressed: () {
-                //AppBloc.get(context).userLogin();
-              },
-              child: const Text('Login'),
+        return
+
+        Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          CarouselSlider.builder(
+            itemCount: 3,
+            itemBuilder:
+                (BuildContext context, int itemIndex, int pageViewIndex) =>
+                    Padding(
+              padding: const EdgeInsets.only(top: 18.0),
+              child: Container(
+                  clipBehavior: Clip.antiAlias,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      border: Border.all(color: Colors.black12)),
+                  width: double.infinity,
+                  child: Image.asset('assets/img/home/hotel_image.jpeg',
+                      fit: BoxFit.cover)),
             ),
-            MaterialButton(
-              onPressed: () {},
-              child: const Text('Get Profile'),
-            ),
-            MaterialButton(
-              onPressed: () {},
-              child:   Text('Get Hotels',style:openSans(20.sp, Colors.black, FontWeight.bold) ,),
-            ),
-          ],
-        );
+            options: CarouselOptions(
+                height: 200,
+                autoPlay: true,
+                initialPage: 1,
+                viewportFraction: 1,
+                scrollDirection: Axis.horizontal,
+                clipBehavior: Clip.antiAliasWithSaveLayer),
+          )
+        ]
+              );
       },
     );
   }
